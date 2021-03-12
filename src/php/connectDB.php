e8dff5b289a6aa7cb0bf6136c341ce708e7a041a
Swaraj Pradhan <tts312@outlook.com>
2021-03-12 20:18:09 +0530

    more products added

--- src/php/connectDB.php
+++ src/php/connectDB.php
@@ -1,11 +1,10 @@
 <?php
 
-$link = (function(){
-    // $parts = (parse_url(getenv('DATABASE_URL')));
-    // extract($parts);
-    // $path = ltrim($path, "/");
-    // return pg_connect("host={$host} dbname={$path} user={$user} password={$pass}");
-    return pg_connect("host='ec2-52-1-95-247.compute-1.amazonaws.com' dbname='d28i9rkkpta8rv' user='hfjxwjgmweztvw' password='50476365afd5266bf9a00ec8983288bdbc2c03254abd47efb91f014c27c6ec92'");
+$link = (function() {
+    $parts = (parse_url(getenv('DATABASE_URL')));
+    extract($parts);
+    $path = ltrim($path, "/");
+    return pg_connect("host={$host} dbname={$path} user={$user} password={$pass}");
 })();
 /* check connection */
 if (pg_last_error()) {
